b0545aa57895cff3ed03004dec35a53e31b2e3bc
Felipe Soares <felipeluizsoares@gmail.com>
2017-03-05 22:29:41 -0300

    Add Unsplash gem

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,4 @@
+Unsplash.configure do |config|
+  config.application_id = "6a0ab5910c6b1a349c5a73a26a0a29e69a25ae3cde326537d4ecdb1ed4815f36"
+  config.application_secret = "a6b2d90670edafce68d1b3931c54accd34dd6aefc0883e0dc4871ccb2ca657b5"
+end
